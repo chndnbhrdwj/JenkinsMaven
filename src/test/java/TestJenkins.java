e5bb8512bd6fdbc2d6c822ef4e3a54828b9c8bc3
Chandan Kumar <chandan.bhardwaj22@gmail.com>
2015-12-03 16:55:51 +0000

    Initial commit

---
+++ src/test/java/TestJenkins.java
@@ -0,0 +1,46 @@
+import org.junit.*;
+
+/**
+ * Created by cku04 on 03/12/2015.
+ */
+public class TestJenkins {
+
+    @BeforeClass
+    public void beforeClassMethod(){
+        System.out.println("In a before class");
+    }
+
+    @Before
+    public void beforeMethod(){
+        System.out.println("In a before method");
+    }
+    @After
+    public void afterMethod(){
+        System.out.println("In a after method");
+    }
+    @AfterClass
+    public void afterClassMethod(){
+        System.out.println("In a after class");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("In a test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("In a test2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("In a test3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("In a test4");
+    }
+
+}
